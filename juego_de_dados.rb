34cad4241f7b32925e325bb502bfd559419e2c1a
dion77 <inge_castaneda@hotmail.com>
2013-05-04 12:27:11 -0700

    Creacion de mensaje y variables

--- juego_de_dados.rb
+++ juego_de_dados.rb
@@ -1,12 +1,8 @@
-puts "
-
-
-
-
-          Bienvenidos al juego de dados
-
-
-
-
-
-"
+puts "Bienvenidos al juego de dados"
+puts "Hola, como te llamas:"
+nombre = gets.chomp
+puts "Hola "+ nombre +" vamos a jugar..."
+a = rand(6)+1
+puts "Hola #{nombre} tu dado dio #{a}"
+b = rand(6)+1
+puts "La computadora tiene #{b}"
