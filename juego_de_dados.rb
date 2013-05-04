7cf9d57bb8bf20022c1b74978d33c5b4ce676180
dion77 <inge_castaneda@hotmail.com>
2013-05-04 13:13:48 -0700

    Ya acabe

--- juego_de_dados.rb
+++ juego_de_dados.rb
@@ -6,3 +6,11 @@ a = rand(6)+1
 puts "Hola #{nombre} tu dado dio #{a}"
 b = rand(6)+1
 puts "La computadora tiene #{b}"
+if a>b
+   puts "Ganaste #{nombre}"
+elsif a<b
+   puts "Perdiste #{nombre} looooser"
+else a=b
+   puts "Empataste"
+end
+
